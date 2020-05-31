4010ee3d576c0fd8d65a6cb3474ae55af84e4644
davibispo <davi-bispo@hotmail.com>
2020-05-31 17:30:55 -0300

    tentando colocar paginas a parte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,14 +46,16 @@
             </div>
         </header>
         <!-- About-->
-        <section class="page-section bg-primary" id="about">
+        <section class="page-section about" id="about">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">We've got what you need!</h2>
+                        <h2 class="text-white mt-0">Nós temos o que você precisa!</h2>
                         <hr class="divider light my-4" />
-                        <p class="text-white-50 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
-                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+                        <p class="text-white-50 mb-4">
+                            RIGOR SOLUÇÕES AMBIENTAIS é uma empresa de consultoria, serviços, execução de projetos e treinamentos em temáticas ambientais e de sustentabilidade.
+                        </p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href=" {{route('empresa.index')}} ">CONHEÇA MAIS!</a>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('empresa', 'EmpresaController');
+
 Route::get('/', function () {
     return view('welcome');
 });
