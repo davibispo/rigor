6991e1aebe833746ce217ce550591926877108a7
davibispo <davi-bispo@hotmail.com>
2020-06-25 20:09:28 -0300

    link Home no nav e mudança da font-family

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,7 @@
                 <a class="navbar-brand js-scroll-trigger" href=" {{route('home.index')}} "><img class="img-responsive" src="assets/img/logo-horizontal.png" width="230px"></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{route('home.index')}}">Home</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{route('empresa.index')}}">A Empresa</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{route('servicos.index')}}">Serviços</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{route('noticias.index')}}">Notícias</a></li>
